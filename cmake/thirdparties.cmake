a4f7ccf208c2202d24c3e65ae192b423e95e8dc5
d1rebear <19630734+d1rebear@users.noreply.github.com>
2020-01-08 23:17:23 +0300

    thirdparties: update find boost parameters

--- cmake/thirdparties.cmake
+++ cmake/thirdparties.cmake
@@ -2,10 +2,9 @@ cmake_minimum_required( VERSION 3.12 )
 
 set( BOOST_ROOT "" CACHE PATH "Boost library path" )
 
-set( Boost_NO_SYSTEM_PATHS ON CACHE BOOL "Do not search system for Boost" )
 set( Boost_USE_STATIC_LIBS ON )
 
-find_package( Boost 1.72 REQUIRED
+find_package( Boost REQUIRED
     COMPONENTS
     chrono
     program_options
